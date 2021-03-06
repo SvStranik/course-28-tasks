c9bf891a6ec927cb2897744c466e4c1ed3285880
Svyatoslav <77211900+SvStranik@users.noreply.github.com>
2021-03-06 22:25:39 +0300

    Create MassVote.py

---
+++ MassVote.py
@@ -0,0 +1,12 @@
+def MassVote(N,Votes):
+    s1 = 'majority winner'
+    s2 = 'minority winner'
+    s3 = 'no winner'
+    sortVotes = sorted(Votes, reverse = True)
+    maxVotes = sortVotes[0] 
+    if N > 1 and sortVotes[0] == sortVotes[1]: return s3
+    resultVotes = round((sortVotes[0] * 100 / sum(Votes)),3) 
+    for i in range(N):
+        if Votes[i] == sortVotes[0]: position = ' ' + str(i+1)
+    if resultVotes > 50: return s1 + position
+    else: return s2 + position
