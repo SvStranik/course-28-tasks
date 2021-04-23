02158813a5152079b55b287eeb8878ed62485376
SvStranik <svstranik@gmail.com>
2021-04-23 17:50:40 +0300

    modified: TransformTransform.py

--- TransformTransform.py
+++ TransformTransform.py
@@ -1,12 +1,12 @@
-def Transform(A,N):
+def Transform(A):
     B = []
-    for i in range(0,N-1):
-        for j in range(0,N-i-1):
+    for i in range(len(A)-1):
+        for j in range(len(A)-i-1):
             k = i + j
             B.append(max(A[j:k+1]))
     return B
 def TransformTransform(A,N):
-    A = Transform(A,N)
-    B = Transform(A,N)
+    A = Transform(A)
+    B = Transform(A)
     if sum(B) % 2 == 0: return True
     else: return False
