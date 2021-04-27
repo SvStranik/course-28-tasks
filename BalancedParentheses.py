3dd10e3af075c019ea688829009ec27b728cc5f6
SvStranik <svstranik@gmail.com>
2021-04-27 14:14:25 +0300

    modified: BalancedParentheses.py

--- BalancedParentheses.py
+++ BalancedParentheses.py
@@ -1,11 +1,10 @@
-def string1(N):
-    if N == 1: return "()"
-    return "(" + string1(N-1) + ")"
-def string2(N):
-    if N == 1: return "("+")"
-    return string2(N-1) + "()"
 def BalancedParentheses(N):
-    if N == 1: return "()"
-    S = string2(N)
-    N = string1(N)
-    return N +' '+ S
+    if N == 1: return '()'
+    if N == 2: return '(()) ()()'
+    per = BalancedParentheses(N-1).split()
+    per.append(per[0])
+    per[0] = '(' + per[0] + ')'
+    for i in range(1,len(per)):
+        per[i] = per[i] + '()'
+    N = ' '.join(per)
+    return N
