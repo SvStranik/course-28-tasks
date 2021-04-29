eebb7c358c40168455f4060abf85431fa789bb83
SvStranik <svstranik@gmail.com>
2021-04-29 18:16:02 +0300

    modified:   BalancedParentheses.py

--- BalancedParentheses.py
+++ BalancedParentheses.py
@@ -1,14 +1,30 @@
 def BalancedParentheses(N):
-    if N == 1: return '()'
-    if N == 2: return '(()) ()()'
-    per = BalancedParentheses(N-1).split()
-    per2 = []
-    for i in per:
-        if i[0] + '()' + i[1:] not in per2:
-            per2.append(i[0] + '()' + i[1:])
-        if i[:2] + '()' + i[2:] not in per2:
-            per2.append(i[:2] + '()' + i[2:])
-        if '()' + i not in per2: 
-            per2.append('()' + i)
-    N = ' '.join(per2)
-    return N
+    a = 0
+    variants = []
+    if N > 0:
+        variant = ['(', 1, 1]
+        variants.append(variant)
+        while a == 0:
+            a = 1
+            for v in variants:
+                if v[1] < N and v[2] > 0:
+                    a = 0
+                    variants.append([v[0] + ')', v[1], v[2] - 1])
+                    v[0] += '('
+                    v[1] += 1
+                    v[2] += 1
+                elif v[1] == N and v[2] > 0:
+                    a = 0
+                    v[0] += ')'
+                    v[2] -= 1
+                elif v[1] < N and v[2] == 0:
+                    a = 0
+                    v[0] += '('
+                    v[1] += 1
+                    v[2] += 1
+    else:
+        return []
+    for i in variants:
+        variants[variants.index(i)] = i[0]
+    return ' '.join(variants)
+    
