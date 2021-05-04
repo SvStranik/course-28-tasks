1e9bf257e8b46b86af84e69a801f31f43057b450
SvStranik <svstranik@gmail.com>
2021-05-04 21:13:28 +0300

    modified:   Football.py

--- Football.py
+++ Football.py
@@ -5,19 +5,20 @@ def Football(F,N):
             if len(F) - i == 2:
                 return True
             elif F[i+1] < F[i+2]:
-                for j in range(i+1,len(F)-i):
-                    if F[j] > F[j+1]:
-                        F[i],F[j+1] = F[j+1],F[i]
+                counter = i+1
+                while F[counter] < F[counter+1]:
+                    counter += 1
+                    if counter == len(F)-1: 
                         break
+                else:
+                    F[i],F[counter+1] = F[counter+1],F[i]
             else:
-                for j in range(i+1,len(F)-i):
-                    if F[j] < F[j+1]:
-                        F[i:j+1] = list(reversed(F[i:j+1]))
-                        break
-                    if len(F)-i-1 == j:
-                        F[i:] = list(reversed(F[i:]))
+                counter = i
+                while counter != len(F)-1 and F[counter] > F[counter+1]:
+                        counter += 1
+                else:
+                    F[i:counter+1] = list(reversed(F[i:counter+1]))                                    
             resultat = True
         if F[i] > F[i+1] and resultat == True:
             return False
     return resultat
-    
